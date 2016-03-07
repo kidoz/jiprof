358baf9c58299084ca6eb78eb4b6ecd8135254fe
kidoz <ckidoz@gmail.com>
2016-03-07 23:27:48 +0300

    Migrated to @Test

--- jip/src/test/java/com/mentorgen/tools/profile/runtime/MetricsTest.java
+++ jip/src/test/java/com/mentorgen/tools/profile/runtime/MetricsTest.java
@@ -1,58 +1,66 @@
 package com.mentorgen.tools.profile.runtime;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class MetricsTest extends TestCase {
-	
-	public void testInitial() {
-		Metrics m = new Metrics();
-		assertTrue(m.getCount() == 0);
-		assertTrue(m.getTotalTime() == 0);
-	}
+import static org.junit.Assert.assertTrue;
 
-	public void testBasic() {
-		final int TIME = 100;
-		Metrics m = new Metrics();
-		m.inc(TIME);
-		assertTrue(m.getCount() == 1);
-		assertTrue(m.getTotalTime() == TIME);
-	}
-	
-	public void testTwo() {
-		final int TIME1 = 100;
-		final int TIME2 = 250;
-		Metrics m = new Metrics();
-		m.inc(TIME1);
-		m.inc(TIME2);
-		assertTrue(m.getCount() == 2);
-		assertTrue(m.getTotalTime() == TIME1 + TIME2);
-	}
-	
-	public void testAdjust() {
-		final int TIME = 100;
-		final int ADJUST = 2;
-		Metrics m = new Metrics();
-		m.inc(TIME);
-		m.adjust(ADJUST);
-		assertTrue(m.getCount() == 1);
-		assertTrue(m.getTotalTime() == TIME-ADJUST);
-	}
-	
-	public void testBoundaryTime() {
-		final int TIME = -1;
-		Metrics m = new Metrics();
-		m.inc(TIME);
-		assertTrue(m.getCount() == 1);
-		assertTrue(m.getTotalTime() == 0);
-	}
-	
-	public void testBoundarAdjust() {
-		final int TIME = 100;
-		final int ADJUST = -1;
-		Metrics m = new Metrics();
-		m.inc(TIME);
-		m.adjust(ADJUST);
-		assertTrue(m.getCount() == 1);
-		assertTrue(m.getTotalTime() == TIME);
-	}
+public class MetricsTest {
+
+    @Test
+    public void testInitial() {
+        Metrics m = new Metrics();
+        assertTrue(m.getCount() == 0);
+        assertTrue(m.getTotalTime() == 0);
+    }
+
+    @Test
+    public void testBasic() {
+        final int TIME = 100;
+        Metrics m = new Metrics();
+        m.inc(TIME);
+        assertTrue(m.getCount() == 1);
+        assertTrue(m.getTotalTime() == TIME);
+    }
+
+    @Test
+    public void testTwo() {
+        final int TIME1 = 100;
+        final int TIME2 = 250;
+        Metrics m = new Metrics();
+        m.inc(TIME1);
+        m.inc(TIME2);
+        assertTrue(m.getCount() == 2);
+        assertTrue(m.getTotalTime() == TIME1 + TIME2);
+    }
+
+    @Test
+    public void testAdjust() {
+        final int TIME = 100;
+        final int ADJUST = 2;
+        Metrics m = new Metrics();
+        m.inc(TIME);
+        m.adjust(ADJUST);
+        assertTrue(m.getCount() == 1);
+        assertTrue(m.getTotalTime() == TIME - ADJUST);
+    }
+
+    @Test
+    public void testBoundaryTime() {
+        final int TIME = -1;
+        Metrics m = new Metrics();
+        m.inc(TIME);
+        assertTrue(m.getCount() == 1);
+        assertTrue(m.getTotalTime() == 0);
+    }
+
+    @Test
+    public void testBoundarAdjust() {
+        final int TIME = 100;
+        final int ADJUST = -1;
+        Metrics m = new Metrics();
+        m.inc(TIME);
+        m.adjust(ADJUST);
+        assertTrue(m.getCount() == 1);
+        assertTrue(m.getTotalTime() == TIME);
+    }
 }
