1cd90779db1dcb67abaf95dc6832c8da340755d1
Aleksandr Pavlov <ckidoz@gmail.com>
2026-03-27 23:17:38 +0300

    Remove developerworks directory

--- developerworks/new-article.sh
+++
@@ -1,7 +0,0 @@
-# See the "Author guidelines" tab at 
-# http://www.ibm.com/developerworks/aboutdw/
-# for more information on how to use this program.
-#!/bin/bash
-
-dwdir=`dirname $0`
-"${dwdir}/tools/dw-newproject.sh" "-t" "article"
