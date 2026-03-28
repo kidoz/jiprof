712fc6c65734496b2b1412a63f69919c8c9028be
Aleksandr Pavlov <ckidoz@gmail.com>
2026-03-28 19:25:35 +0300

    Migrate to typescript config

---
+++ viewer/prettier.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 2,
+};
+
+export default config;
