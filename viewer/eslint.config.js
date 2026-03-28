ee9445fa40741a20013f3f1d4f7bf4a137e6390f
Aleksandr Pavlov <ckidoz@gmail.com>
2026-03-28 19:07:20 +0300

    Add ESLint and Prettier to viewer
    Configure ESLint 10 flat config with typescript-eslint and
    eslint-config-prettier. Add Prettier with lint, lint:fix, format,
    and format:check npm scripts.

---
+++ viewer/eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  { ignores: ["dist/"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+);
